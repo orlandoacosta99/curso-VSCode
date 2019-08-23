8a5e6afb790447e2583b7fc3b031b9bdf64cb2a9
orlando acosta <acostavargasorlando99@gmail.com>
2019-08-22 23:30:28 -0500

    se realizaron cambios en archivos 1 y 2 de primer video

--- src/app/01-edicion-basica/03-comentarios.ts
+++ src/app/01-edicion-basica/03-comentarios.ts
@@ -5,20 +5,20 @@
     Tips:
         ⌘ /
         
-        Ctrl + \
+        Ctrl + k
 */
 
-const a = 10;
-const b = 20;
-const c = { a, b };
+// const a = 10;
+// const b = 20;
+// const c = { a, b };
 
 
-const a = 30;
-const b = 40;
-const c = { a, b };
+// const a = 30;
+// const b = 40;
+// const c = { a, b };
 
 
-console.log( a, b, c );
+// console.log( a, b, c );
 
 // Resultado final
 // Sólo dejar un bloque comentado
